faf15b4dda5e70055811690f0f80bbeb1520e693
Piyush Raj <piyush.189303055@muj.manipal.edu>
2019-09-17 22:07:36 +0530

    init: __main__.py

---
+++ ytapi/__main__.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    description=(
+                'YTAPI is a python API which allows you to get the transcript/subtitle for a given YouTube video.\n'
+                'It also works for automatically generated subtitles. This API is a part of The Abuse Project\n\n'
+                'Usage:\n'
+                'import ytapi\n'
+                'ytapi.YTAPI.get_transcript("Youtube Video ID")\n\n'
+            )
+    print(description)
