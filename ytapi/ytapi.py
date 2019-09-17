be7ee4371e2af38f39fe2a22abb3d2adada8d871
Piyush Raj <piyush.189303055@muj.manipal.edu>
2019-09-17 22:06:27 +0530

    sys: py version based imports cond.

--- ytapi/ytapi.py
+++ ytapi/ytapi.py
@@ -2,6 +2,26 @@ import sys, re
 from xml.etree import ElementTree
 import requests
 
+"""
+Python version based imports
+"""
+
+if sys.version_info.major == 3 and sys.version_info.minor >= 4:
+    # Python 3.4+
+    from html import unescape
+else:
+    if sys.version_info.major <= 2:
+        # Python 2
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
+        import HTMLParser
+        html_parser = HTMLParser.HTMLParser()
+    else:
+        # Python 3.0 <-> 3.3
+        import html.parser
+        html_parser = html.parser.HTMLParser()
+    def unescape(string):
+        return html_parser.unescape(string)
 
 class YTAPI():
     class CouldNotRetrieveTranscript(Exception):
