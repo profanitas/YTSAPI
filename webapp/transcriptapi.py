ee402e964df88bc566b9d7f3795655e03c9e4f3f
Aniket Chaudhari <anikethc3@gmail.com>
2019-12-20 13:30:44 +0530

    added webapp

---
+++ webapp/transcriptapi.py
@@ -0,0 +1,10 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+def get_subtitles(video_url):
+    video_id = video_url.split('?v=')[1]
+    try:
+        subtitles = YouTubeTranscriptApi.get_transcript(video_id)
+        if type(subtitles) == 'list':
+            return subtitles
+    except Exception:
+        return {"message":"Error retrieving subtitles"}
